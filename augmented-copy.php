0df53db225b94600f613571eb97b04c32faf5c8d
cgutteridge <christophergutteridge@gmail.com>
2021-12-16 11:36:20 +0000

    to 0.4

--- augmented-copy.php
+++ augmented-copy.php
@@ -1,20 +1,20 @@
 <?php
 /**
  * @package Augmented Copy
- * @version 0.3
+ * @version 0.4
  */
 /*
 Plugin Name: Augmented Copy
 Plugin URI: https://github.com/cgutteridge/augmented-copy
 Description: Javascript to add citations and other niftiness to copied text.
 Author: Christopher Gutteridge
-Version: 0.3
+Version: 0.4
 Author URI: http://www.ecs.soton.ac.uk/people/cjg
 */
 
 function augmented_copy_scripts()
 {
-    wp_register_script( 'augmented-copy-script', plugins_url( '/augmented-copy.js', __FILE__ ), array( 'jquery' ), '0.3', true );
+    wp_register_script( 'augmented-copy-script', plugins_url( '/augmented-copy.js', __FILE__ ), array( 'jquery' ), '0.4', true );
     wp_enqueue_script( 'augmented-copy-script' );
 }
 add_action( 'wp_enqueue_scripts', 'augmented_copy_scripts' );
